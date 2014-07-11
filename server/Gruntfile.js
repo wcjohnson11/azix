c1aeee194cc83c4f081104087e93c465dfc44a8a
jlburkhead <jlburkhead@ucdavis.edu>
2014-07-10 19:58:25 -0700

    add nodemon and watch to Gruntfile.js

--- server/Gruntfile.js
+++ server/Gruntfile.js
@@ -2,15 +2,51 @@ module.exports = function(grunt) {
 
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+
     jshint: {
-      all: ['Gruntfile.js', 'lib/**/*.js']
+      all: ['*.js', 'lib/**/*.js']
+    },
+
+    mochaTest: {
+      test: {
+        options: {
+          reporter: 'spec'
+        },
+        src: ['test/**/*.js']
+      }
+    },
+
+    watch: {
+      scripts: {
+        files: ['lib/**/*.js']
+      }
+    },
+
+    nodemon: {
+      dev: {
+        script: 'server.js'
+      }
     }
+
   });
 
 
   grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-mocha-test');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-nodemon');
 
+  grunt.registerTask('serve', function() {
+    var nodemon = grunt.util.spawn({
+      cmd: 'grunt',
+      grunt: true,
+      args: 'nodemon'
+    });
+    nodemon.stdout.pipe(process.stdout);
+    nodemon.stderr.pipe(process.stderr);
 
-  grunt.registerTask('default', ['jshint']);
+    grunt.task.run(['watch']);
+  });
+  grunt.registerTask('default', ['jshint', 'mochaTest']);
 
 };
