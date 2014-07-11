5947c8271b372eccea03899d4e193d02481031f1
jlburkhead <jlburkhead@ucdavis.edu>
2014-07-11 14:45:47 -0700

    remove config

--- server/lib/config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  host: process.env.HOST || 'localhost',
-  dbhost: process.env.DBHOST || 'localhost',
-  dbname: 'azix',
-  bareRepo: 'assets/bareRepo.git',
-  repositories: '/tmp/repositories'
-};
