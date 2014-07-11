6028c205ae429a8efbc28f2428fc761008ff0819
Hack Reactor Students <accounts@hackreactor.com>
2014-07-10 18:47:09 -0700

    refactored config

--- cli/commands/config.js
+++ cli/commands/config.js
@@ -7,24 +7,13 @@ var getUserHome = function() {
   return process.env[(process.platform === 'win32') ? 'USERPROFILE' : 'HOME'];
 };
 
-
 // Global variables storing necessary paths
 var homepath = getUserHome();
-var asixPath = path.join(homepath, '.asix');
+var azixPath = path.join(homepath, '.azixconfig');
 
 
 // Global object that stores user information
 var userInformation = {};
-
-
-// Exported function that creates the global asix folder
-var config = function () {
-  if (!fs.existsSync(asixPath)) {
-    fs.mkdirSync(asixPath);
-  }
-  configQuestions();
-};
-
 
 // Prompts user for information (username, password) and saves them to global variable
 // HIGHLY UNSAFE AS PASSWORDS ARE IN PLAINTEXT
@@ -33,17 +22,23 @@ var configQuestions = function() {
     {
       type: "input",
       name: "username",
-      message: "Please input your asix username"
+      message: "Please input your azix username"
     },
     {
       type: "password",
-      message: "Please input your asix password",
-      name: "password"
+      name: "password",
+      message: "Please input your azix password"
     }
   ];
   inquirer.prompt(questions, function(answers) {
     userInformation = answers;
+    fs.writeFileSync(azixPath, JSON.stringify(userInformation));
   });
+};
+
+// Exported function that creates the global azix folder
+var config = function () {
+  configQuestions();
 };
 
 module.exports = config;
