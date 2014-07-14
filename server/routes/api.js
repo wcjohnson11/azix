c988dbefe88f5cb3e1fb3a9597b36959901023f3
jlburkhead <jlburkhead@ucdavis.edu>
2014-07-14 12:04:18 -0700

    add run api endpoint to routes/api.js

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,7 +1,9 @@
 var express = require('express');
 var router = express.Router();
 var init = require('../lib/init.js');
+var run = require('../lib/run.js');
 
 router.post('/init', init);
+router.post('/run', run);
 
 module.exports = router;
