1800039dcee3dea922dc78b8f8b6d4a18d6d6784
jlburkhead <jlburkhead@ucdavis.edu>
2014-07-16 18:30:21 -0700

    ya ami

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ module.exports = {
   dbname: 'azix',
   bareRepo: 'assets/bareRepo.git',
   repositories: '/tmp/repos',
-  ami: 'ami-eaef2382', // Ubuntu 12.04 LTS
+  ami: 'ami-66e8240e', // Ubuntu 12.04 LTS
   awsAccessKeyId: process.env.AWS_ACCESS_KEY_ID,
   awsSecretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
   ec2RepoPath: '/tmp/repo'
