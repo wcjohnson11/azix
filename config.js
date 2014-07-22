db1b82ead5be48ec6a6d15d79a9605efc0f667a3
jlburkhead <jlburkhead@ucdavis.edu>
2014-07-21 19:57:41 -0700

    update config repos

--- config.js
+++ config.js
@@ -1,14 +1,15 @@
 // mongodb://<dbuser>:<dbpassword>@ds059938.mongolab.com:59938/azix
+var path = require('path');
 
 module.exports = {
   host: process.env.HOST || 'localhost',
   port: process.env.PORT || 8000,
-  dbhost: process.env.DBHOST || 'ds059938.mongolab.com:59938',
+  dbhost: process.env.DBHOST || 'localhost',
   dbuser: 'azix',
   dbpassword: process.env.DBPASSWORD || 'azix',
   dbname: 'azix',
   bareRepo: 'assets/bareRepo.git',
-  repositories: process.env.NODE_ENV === 'prod' ? '/home/ubuntu/repos' : '/tmp/repos',
+  repositories: process.env.NODE_ENV === 'prod' ? path.join(process.env.HOME, 'repos') : '/tmp/repos',
   ami: 'ami-0e36f866', // Ubuntu 12.04 LTS
   awsAccessKeyId: process.env.AWS_ACCESS_KEY_ID,
   awsSecretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
