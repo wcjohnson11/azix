72babeff7d5b043fff80ac0a87f513749e3ec9ec
jlburkhead <jlburkhead@ucdavis.edu>
2014-07-21 17:25:02 -0700

    update config.repositories

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ module.exports = {
   dbhost: process.env.DBHOST || 'localhost',
   dbname: 'azix',
   bareRepo: 'assets/bareRepo.git',
-  repositories: '/tmp/repos',
+  repositories: process.env.NODE_ENV === 'prod' ? '/home/ubuntu/repos' : '/tmp/repos',
   ami: 'ami-0e36f866', // Ubuntu 12.04 LTS
   awsAccessKeyId: process.env.AWS_ACCESS_KEY_ID,
   awsSecretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
