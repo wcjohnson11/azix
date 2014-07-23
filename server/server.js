52f74b65f40be1c2e65874cf7f17932fde52e08c
wcjohnson11 <williamcouperjohnson@gmail.com>
2014-07-23 10:24:52 -0700

    add function to retrieve list of projects

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 var config = require('../config.js');
 var apiRouter = require('./routes/api.js');
 var gitRouter = require('./routes/git.js');
+var webRouter = require('./routes/web.js');
 var express = require('express');
 var bodyParser = require('body-parser');
 var logger = require('morgan');
@@ -13,6 +14,7 @@ app.use(express.static(__dirname + '/public'));
 
 app.use('/api', apiRouter);
 app.use('/repos', gitRouter);
+app.use('/', webRouter);
 
 app.listen(process.env.PORT || config.port);
 
