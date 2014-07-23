0f1863f1946c0100f093e03ca9f47b9ab0671c09
wcjohnson11 <williamcouperjohnson@gmail.com>
2014-07-23 10:35:48 -0700

    get ready for ui-routing

--- server/server.js
+++ server/server.js
@@ -1,7 +1,6 @@
 var config = require('../config.js');
 var apiRouter = require('./routes/api.js');
 var gitRouter = require('./routes/git.js');
-var webRouter = require('./routes/web.js');
 var express = require('express');
 var bodyParser = require('body-parser');
 var logger = require('morgan');
@@ -14,7 +13,6 @@ app.use(express.static(__dirname + '/public'));
 
 app.use('/api', apiRouter);
 app.use('/repos', gitRouter);
-app.use('/', webRouter);
 
 app.listen(process.env.PORT || config.port);
 
