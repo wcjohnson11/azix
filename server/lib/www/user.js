c862cb95b4d616625353416cf9c65d660c8c27fe
Walker Mellema <walkermellema@gmail.com>
2014-07-25 11:37:26 -0700

    finish user login (complete!)

--- server/lib/www/user.js
+++ server/lib/www/user.js
@@ -3,23 +3,24 @@ var db = require('../../db/config.js');
 // Grab the list of projects from the database RunLog
 var userHandler = function(req, res) {
   console.log(req.body);
-  db.User.find({ username: req.body.username }, function(err, user) {
+  db.User.findOne({ username: req.body.username }, function(err, user) {
     if (err) {
       console.log(err);
       res.send('Error');
     } else {
       if (!user) {
         console.log(user);
-        db.User.create({username: req.body.username, password: "hi"}, function(err, user){
+        db.User.create({username: req.body.username, password: req.body.password}, function(err, user){
           if (err) {
             console.log(err)
           } else {
             res.json(201, user)
           }
         });
+      }else{
+        return res.json(200, user);
       }
       //maybe we have to return res.send instead?
-      return res.json(200, user);
     }
   });
 
