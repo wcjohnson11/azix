968e1ea391c12848259e84c71b32fd7b6109ee2a
Walker Mellema <walkermellema@gmail.com>
2014-07-25 11:07:54 -0700

    rebasing with add login form

--- server/public/app/app.js
+++ server/public/app/app.js
@@ -41,16 +41,16 @@ angular.module('app', ['ui.router', 'factories','ngTable','controllers', 'angula
 
 
 .directive('loadingContainer', function () {
-    return {
-        restrict: 'A',
-        scope: false,
-        link: function(scope, element, attrs) {
-            var loadingLayer = angular.element('<div class="loading"></div>');
-            element.append(loadingLayer);
-            element.addClass('loading-container');
-            scope.$watch(attrs.loadingContainer, function(value) {
-                loadingLayer.toggleClass('ng-hide', !value);
-            });
-        }
-    };
+  return {
+    restrict: 'A',
+    scope: false,
+    link: function(scope, element, attrs) {
+      var loadingLayer = angular.element('<div class="loading"></div>');
+      element.append(loadingLayer);
+      element.addClass('loading-container');
+      scope.$watch(attrs.loadingContainer, function(value) {
+          loadingLayer.toggleClass('ng-hide', !value);
+      });
+    }
+  };
 });
