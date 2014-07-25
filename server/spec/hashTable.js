c299da1efa5875ba5f66a85be399a2631e724161
Mike <mbarton.mail@gmail.com>
2014-07-25 09:25:43 -0700

    Change spec folder

---
+++ server/spec/hashTable.js
@@ -0,0 +1,25 @@
+/* global hashTable, describe, it, expect, should */
+
+describe('hashTable()', function () {
+  'use strict';
+  var hi;
+
+  it('exists', function () {
+    expect(makeHashTable).to.be.a('function');
+
+  });
+
+  beforeEach(function(){
+    hi = "hi";
+  });
+
+  it('does something', function () {
+    expect(true).to.equal(false);
+  });
+
+  it('does something else', function () {
+    expect(true).to.equal(false);
+  });
+
+  // Add more assertions here
+});
